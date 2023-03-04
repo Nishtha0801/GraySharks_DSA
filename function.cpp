9a80ce4bad3a7239647df2a7c6c08ad8b839ec85
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-04 14:20:00 +0530

    functions

---
+++ function.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+// void greeting(){
+//     cout<<"Hello world!";
+//     return;
+// }
+
+// void greet(){
+//     cout<<"Yahn bhi ho skta h na mam"<<endl;
+//     return;
+// }
+
+// int printNumber(int num){
+//     cout<<num<<endl;
+//     return 0;
+// }
+
+void greet();
+
+int main(){
+    greet();
+    // greeting();
+    // printNumber(4);
+    return 0;
+}
+
+void greet(){
+     cout<<"Hello world!";
+     return;
+}
+
+
--- helloworld.cpp
+++ helloworld.cpp
@@ -239,5 +239,51 @@ int main(){
                         cout << endl;
                     }
 
+
+                // Homework pattern
+                int n;
+                cout<<"Enter number: ";
+                cin>>n;
+                
+                int count = 1;
+                for(int row=1; row<=n; row++){
+                    for(int col=1;col<=row;col++){
+                    cout<<count<< " ";
+                    count++;
+                    }
+                    cout<<endl;
+                }
+
+                // Homework Pattern
+                    int n;
+                    cout<<"Enter number: ";
+                    cin>>n;
+                    
+                    int outerSpacing = n/2;
+                    int innerSpacing = -1;
+                    
+                    for(int row=1;row<=n; row++){
+                        for(int col=1; col<=outerSpacing; col++){
+                            cout<<"-";
+                        }
+                        cout<<"*";
+                        for(int col=1; col<=innerSpacing; col++){
+                            cout<<"-";
+                        }
+                        if(row > 1 && row < n){
+                            cout<<"*";
+                        }
+                        
+                        if(row <= n/2){
+                            outerSpacing--;
+                            innerSpacing+=2;
+                        } else{
+                            outerSpacing++;
+                            innerSpacing-=2;
+                        }
+                        cout<<endl;
+                    }
+   
+
     return 0;
 }
