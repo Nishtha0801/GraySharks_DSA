431b0bd30a9456b3ed5d5faa2d7dcf519ee118c5
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-05 15:36:52 +0530

    questions

--- questionArray.cpp
+++ questionArray.cpp
@@ -43,28 +43,66 @@ void reverse(vector<int> &arr, int si, int ei){
     }
 
 }
+
+void addArrays(vector<int> &a, vector<int> &b){
+    int n = a.size();
+    int m = b.size();
+
+    vector<int>res(max(n,m)+1, 0);
+
+    int i = n-1;
+    int j = m-1;
+    int k = res.size() - 1;
+
+    int carry = 0;
+    while(k >= 0){
+        int sum = carry;
+        if(i >= 0){
+            sum+=a[i];
+            i--;
+        }
+        if(j >= 0){
+            sum+=b[j];
+            j--;
+        }
+
+        res[k] = sum%10;
+        k--;
+        carry = sum/10;
+    }
+
+    for(int i=0;i<res.size(); i++){
+        cout<<res[i]<<" ";
+    }
+
+    return;
+}
+
+
 int main(){
     //Q1 Find an element in the array(arr, num)
     //q2 Find the minimum element in the array.
     //q3 Find the maximum element in the array.
 
-    vector<int>myArr(5);
-    for(int i=0; i<myArr.size();i++){
-        cin>>myArr[i]; 
-    }
+    vector<int>myArr = {9,5,4,9};
+    vector<int>arr = {2,1,4};
+    // for(int i=0; i<myArr.size();i++){
+    //     cin>>myArr[i]; 
+    // }
 
-    for(int i=0;i<myArr.size(); i++){
-        cout<<myArr[i]<<" ";
-    }
-    cout<<endl;
+    // for(int i=0;i<myArr.size(); i++){
+    //     cout<<myArr[i]<<" ";
+    // }
+    // cout<<endl;
     // cout<<findElement(myArr, 5);
     // cout<<minimumElement(myArr);
     // cout<<maximumElement(myArr);
     // swap(myArr, 0, myArr.size()-1);
-    reverse(myArr, 0, myArr.size()-1);
-     for(int i=0;i<myArr.size(); i++){
-        cout<<myArr[i]<<" ";
-    }
+    // reverse(myArr, 0, myArr.size()-1);
+    //  for(int i=0;i<myArr.size(); i++){
+    //     cout<<myArr[i]<<" ";
+    // }
+    addArrays(myArr, arr);
 
 
     
