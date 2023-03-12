755017220a1ef45b5f4a08c2c92d97728f7fb2a7
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-12 15:43:05 +0530

    searching

--- searching.cpp
+++ searching.cpp
@@ -15,10 +15,51 @@ int linearSearch(vector<int> &myArr, int data){
     return -1;
 }
 
+int binarySearch(vector<int> &arr, int data){
+    int n = arr.size();
+    int si = 0;
+    int ei = n-1;
+
+    while(si<=ei){
+        int mid = (si + ei)/2;
+        if(arr[mid] == data){
+            return mid;
+        } else if(arr[mid] > data){
+            ei = mid-1;
+        } else {
+            si = mid+1;
+        }
+    }
+    return -1;
+}
+
+int firstIndex(vector<int> &arr, int data){
+    int si = 0;
+    int ei = arr.size()-1;
+
+    while(si <= ei){
+        int mid = (si+ei)/2;
+        if(arr[mid] == data){
+            if(mid-1>=0 && arr[mid-1] == data){
+                ei = mid-1;
+            } else{
+                return mid;
+            }
+        } else if(arr[mid] > data){
+            ei = mid - 1;
+        } else {
+            si = mid+1;
+        }
+    }
+    return -1;
+}
+
 int main(){
    
-    vector<int>myArr = {1,2,3,4,5,6,7};
-    int data = 16;
+    vector<int>myArr = {1,2,2,2,2,2,3,4,5,6,7,8,9,9,9,9};
+    int data = 9;
 
-    cout<<linearSearch(myArr, data)<<endl;
+    // cout<<linearSearch(myArr, data)<<endl;
+    // cout<<binarySearch(myArr, data)<<endl;
+    cout<<firstIndex(myArr, data);
 }
