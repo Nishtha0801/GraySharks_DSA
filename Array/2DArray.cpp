aff7a9d2352b65a0d5a1ba2f02a339746eb28bf6
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-29 21:22:30 +0530

    2d vector

--- Array/2DArray.cpp
+++ Array/2DArray.cpp
@@ -28,13 +28,21 @@ void display(vector<vector<int>> &res)
     }
 }
 
+// maximum ele 
+
+
 int main()
 {
     
     int m = 2;
     int n = 3;
-    vector<vector<int>> res(m, vector<int>(n, 0));
-    input(res,m,n);
+    // vector<vector<int>> res(m, vector<int>(n, 0));
+    // input(res,m,n);
+    vector<vector<int>>res = {
+      {1,2,3},
+      {4,5,6},
+      {7,8,9},  
+    };
     display(res);
     return 0;
 }
