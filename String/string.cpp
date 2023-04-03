c026405ae0f866c7464dcbfc8e546418a7f43a24
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-04-03 23:04:24 +0530

    string

--- String/string.cpp
+++ String/string.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void reverseString(string &str){
+void reverseString(string &str)
+{
     int n = str.length();
     int si = 0;
-    int ei = n-1;
-    while(si<ei){
+    int ei = n - 1;
+    while (si < ei)
+    {
         char temp = str[si];
         str[si] = str[ei];
         str[ei] = temp;
@@ -15,35 +17,120 @@ void reverseString(string &str){
     }
 }
 
-//leetcode 2108
+// leetcode 2108
 
-    bool isPalindrome(string str){
-        int si = 0, ei = str.length()-1;
+bool isPalindrome(string str)
+{
+    int si = 0, ei = str.length() - 1;
 
-        while(si<ei){
-            if(str[si++] != str[ei--]){
-                return false;
-            }
+    while (si < ei)
+    {
+        if (str[si++] != str[ei--])
+        {
+            return false;
         }
+    }
+    return true;
+}
+
+string firstPalindrome(vector<string> &words)
+{
+    int n = words.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isPalindrome(words[i]))
+        {
+            return words[i];
+        }
+    }
+
+    return "";
+}
+
+// leetcode 125
+bool isPalindrome(string s)
+{
+
+    int i = 0, j = s.size() - 1;
+    if (s.size() <= 1)
+    {
         return true;
     }
 
-    string firstPalindrome(vector<string> &words) {
-        int n = words.size();
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
 
-        for(int i=0;i<n;i++){
-            if(isPalindrome(words[i])){
-                return words[i];
-            }
+    while (i < j)
+    {
+        if (!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')))
+        {
+            i++;
+            continue;
         }
-
-        return "";
+        if (!((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= '0' && s[j] <= '9')))
+        {
+            j--;
+            continue;
+        }
+        if (s[i] != s[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
     }
 
-int main(){
+    return true;
+}
+
+void generateSubstring(string &str)
+{
+    int n = str.length();
+    for (int len = 1; len <= n; len++)
+    {
+        for (int i = 0; i <= n - len; i++)
+        {
+            int j = i + len - 1;
+            for (int k = i; k <= j; k++)
+            {
+                cout << str[k];
+            }
+            cout << endl;
+        }
+    }
+}
+
+void generateSubstringTwo(string &str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int len = 1; i + len <= str.length(); len++)
+        {
+            cout << (str.substr(i, len)) << endl;
+        }
+    }
+}
+
+void generatePalindromicSubstrings(string &str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int len = 1; i + len <= str.length(); len++)
+        {
+            string temp = str.substr(i, len);
+            if (isPalindrome(temp))
+            {
+                cout << temp << endl;
+            }
+        }
+    }
+}
+
+int main()
+{
     string str = "Nishtha Goyal";
     reverseString(str);
-    cout<<str<<endl;
+    cout << str << endl;
     // cout<<str[2]<<endl;
 
     // char arr[5] = {'a', 'e', 'i', 'o', 'u'};
@@ -54,11 +141,4 @@ int main(){
     // // reverse a string
 
     // cout<<fullName<<endl;
-
-
-
-
-
-
-
 }
