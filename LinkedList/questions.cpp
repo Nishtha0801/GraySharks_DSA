acfeb2f0ba5daf3e6b0a85bbc9d7b66868fe39cb
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-05-13 12:53:03 +0530

    ll

--- LinkedList/questions.cpp
+++ LinkedList/questions.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
-  
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+  //leetcode 876
   ListNode* middleNode(ListNode* head) {
         if(head == nullptr || head -> next == nullptr){
             return head;
