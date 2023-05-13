87362428af10e1149f58c772fa9f5303e200e016
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-05-13 14:55:02 +0530

    ll

--- LinkedList/questions.cpp
+++ LinkedList/questions.cpp
@@ -23,7 +23,7 @@ struct ListNode {
         return slow;
     }
 
-    // leetcode 206
+    // leetcode 206 ====> by reversing
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
         ListNode* curr = head;
@@ -37,3 +37,75 @@ struct ListNode {
         }
         return prev;
     }
+
+
+    // leetcode 206 by swapping the data ============
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode2( ListNode* head){
+        if(head == nullptr || head -> next == nullptr){
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            ListNode* forw = curr->next;
+
+            //linking
+            curr->next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+
+    void reverseData(ListNode* head) {
+       if(head == nullptr || head->next == nullptr){
+           return;
+       }
+       ListNode* mid = middleNode2(head);
+       ListNode* thead = mid->next;
+
+       mid->next = nullptr;
+
+       thead = reverseList(thead);
+       ListNode* c1 = head;
+       ListNode* c2 = thead;
+
+       while(c1 != nullptr && c2 != nullptr){
+           int temp = c1->val;
+           c1->val = c2->val;
+           c2->val = temp;
+
+           c1 = c1->next;
+           c2 = c2->next;
+       }
+
+       thead = reverseList(thead);
+       mid->next = thead;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        reverseData(head);
+       return head;
+    }
+};
