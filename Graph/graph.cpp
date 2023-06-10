211e4584e45f0cf447eb87b92a8b77952b3b287e
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-06-10 12:47:10 +0530

    graph

---
+++ Graph/graph.cpp
@@ -0,0 +1,63 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Edge
+{
+public:
+    int v = 0;
+    int w = 0;
+
+    Edge(int v, int w)
+    {
+        this->v = v;
+        this->w = w;
+    }
+};
+
+int N = 7;
+vector<vector<Edge>> graph(N, vector<Edge>());
+
+void addEdge(int u, int v, int w)
+{
+    graph[u].push_back(Edge(v, w));
+    graph[v].push_back(Edge(u, w));
+}
+
+// display ============
+
+void display()
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << (to_string(i) + "->");
+        for (Edge e : graph[i])
+        {
+            cout << "(" + to_string(e.v) + "," + to_string(e.w) + ")";
+        }
+        cout << endl;
+    }
+}
+
+void constructGraph()
+{
+    addEdge(0, 1, 10);
+    addEdge(0, 3, 10);
+    addEdge(1, 2, 10);
+    addEdge(2, 3, 10);
+    addEdge(3, 4, 2);
+    addEdge(4, 5, 2);
+    addEdge(4, 6, 8);
+    addEdge(5, 6, 3);
+
+    display();
+}
+
+int main()
+{
+    constructGraph();
+
+    return 0;
+}
