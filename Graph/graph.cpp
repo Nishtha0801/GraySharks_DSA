080e7436403c851762c451f99375d1f15b8faf5f
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-06-10 15:50:29 +0530

    graph

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -41,12 +41,73 @@ void display()
     }
 }
 
+int searchVtx(int u, int v){
+    for(int i=0;i<graph[u].size(); i++){
+        Edge e = graph[u][i];
+        if(e.v == v){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void removeEdge(int u, int v){
+    int l1 = searchVtx(u,v);
+    graph[u].erase(graph[u].begin() + l1);
+    
+    int l2 = searchVtx(v,u);
+     graph[v].erase(graph[v].begin() + l2);
+}
+
+void removeVtx(int u){
+    for(int i= graph[u].size()-1; i>=0;i--){
+        Edge e = graph[u][i];
+        removeEdge(u, e.v);
+    }
+}
+
+bool hasPath(int src, int dest, vector<bool>&vis){
+    // try krengeee
+    if(src == dest){
+        return true;
+    }
+    
+    vis[src] = true;
+    bool res = false;
+    // to traverse over the neighbours
+    for(Edge e : graph[src]){
+        if(!vis[e.v]){
+            res = res || hasPath(e.v, dest, vis);
+        }
+    }
+    return res;
+}
+
+//psf -> path so far
+int allPaths(int src, int dest, vector<bool>&vis, string psf, int wsf){
+    // try krengeeee
+    if(src == dest){
+        cout<<psf + to_string(src) + "@ " + to_string(wsf) <<endl;
+        return 1;
+    }
+    
+    int count = 0;
+    vis[src] = true;
+    for(Edge e: graph[src]){
+        if(!vis[e.v]){
+            count+=allPaths(e.v, dest, vis, psf+to_string(src)+" ", wsf+e.w);
+        }
+    }
+    vis[src] = false;
+    return count;
+}
+
 void constructGraph()
 {
     addEdge(0, 1, 10);
     addEdge(0, 3, 10);
     addEdge(1, 2, 10);
-    addEdge(2, 3, 10);
+    addEdge(2, 3, 40);
     addEdge(3, 4, 2);
     addEdge(4, 5, 2);
     addEdge(4, 6, 8);
@@ -58,6 +119,16 @@ void constructGraph()
 int main()
 {
     constructGraph();
-
+    vector<bool>vis(N,false);
+    // cout<<hasPath(0,6,vis)<<endl;
+    cout<<allPaths(0,6,vis,"",0);
+    // removeEdge(3,4);
+    // cout<<endl;
+    // display();
+    // removeVtx(3);
+    // cout<<endl;
+    // display();
+    
+    
     return 0;
 }
