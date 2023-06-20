2876f180cc680b7f7adabe8381eeebf4ae0adb90
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-06-20 22:59:15 +0530

    graphs

--- Graph/questions.cpp
+++ Graph/questions.cpp
@@ -32,3 +32,38 @@ public:
         return count;
     }
 };
+
+// leetcode 695
+
+class Solution {
+public:
+
+        int maxAreaOfIslandDFS(vector<vector<int>>& grid, int r, int c, vector<vector<int>>&dir){
+        grid[r][c] = 0;
+        int count = 0;
+        for(int d=0; d<dir.size(); d++){
+            int x = r + dir[d][0];
+            int y = c + dir[d][1];
+
+            if(x>=0 && y>=0 && x<grid.size() && y<grid[0].size() && grid[x][y] == 1){
+                count+=maxAreaOfIslandDFS(grid, x, y, dir); 
+            }
+        }
+        return count+1;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        vector<vector<int>>dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        int maxArea = 0;
+
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] == 1){
+                    maxArea = max(maxArea, maxAreaOfIslandDFS(grid, i, j, dir));
+                    
+                }
+            }
+        }
+        return maxArea;
+    }
+};
